b558a3a06f168d278e931d1c08af016641679c58
William Bryant <william.steen.bryant@gmail.com>
2017-06-12 16:20:07 -0400

    Portfolio on different Client

---
+++ README.md
@@ -0,0 +1,17 @@
+## Building a Portfolio Website with HTML, CSS, jQuery
+###Thanks for stalking My Super Awesome Site/Readme.md/GitHub Profile!
+###Using Github as project base for commiting to Ubuntu AWS Server
+![alt text](img/ss.png "Description goes here")
+<!--![My image](Willyb15.github.com/photo-gallery/Images/image1.jpg)-->
+###Created Layout With BootStrap 
+#####Home, Story, Deep Thoughts, Projects, and Connect Sections
+#####Added Black Headers Before Each Section
+###Styled Each Section using SCSS/CSS
+#####Implemented sweet page-load sequence using CSS animations
+#####Implemented wow animations on content of individual sections
+###Updated Projects Sections
+#####Added projects with links and css hover animations into Projects section
+###Implemented php mailer in AWS
+#####In connect section used a Php Mailer.
+#####Dowloaded php mailer inside Ubuntu AWS server
+###Deployed Project To AWS
---
+++ js/scripts.js
@@ -0,0 +1,98 @@
+// Full Screens the Loader Page then Auto Scrolls to top of page
+setTimeout(function() {
+    $('#loader').hide();
+    $(window).scrollTop(0);
+}, 3000);
+var intFrameHeight = window.innerHeight;
+
+// This is Where the #home Text Fades In
+setTimeout(function() {
+    $('.willb').css("visibility", "visible").hide().fadeIn(6000);
+}, 3500);
+setTimeout(function() {
+    $('.webdev').css("visibility", "visible").hide().fadeIn(6000);
+}, 4000);
+setTimeout(function() {
+    $(".design").css("visibility", "visible").hide().fadeIn(6000);
+}, 6000);
+setTimeout(function() {
+    $(".vertical-divider").css("visibility", "visible").hide().fadeIn(5000);
+}, 5500);
+setTimeout(function() {
+    $(".code").css("visibility", "visible").hide().fadeIn(6000);
+}, 7000);
+setTimeout(function() {
+    $(".build").css("visibility", "visible").hide().fadeIn(6000);
+}, 8000);
+setTimeout(function() {
+    $(".icon").css("visibility", "visible").hide().fadeIn(6000);
+}, 3500);
+
+setTimeout(function() {
+setInterval(function(){
+$('.overlay').addClass('wow pulse animated');
+}, 15000);
+}, 1000);
+setTimeout(function() {
+setInterval(function(){
+$('.overlay').removeClass('wow pulse animated');
+}, 15300);
+}, 1000);
+setInterval(function(){
+$('.logo-img').addClass('wow tada animated');
+}, 8000);
+setInterval(function(){
+$('.logo-img').removeClass('wow tada animated');
+}, 9000);
+
+// setInterval(function(){
+// $('.revolving-row').addClass('wow zoomOutDown animated');
+// }, 13000);
+// setInterval(function(){
+// $('.revolving-row').removeClass('wow zoomOutDown animated');
+// }, 14000);
+
+// .addClass('wow zoomInUp animated');
+// }, 13000);
+
+// .removeClass('wow zoomOutDown animated').removeClass('wow zoomInUp animated');
+// Fixes n
+// Fixes navbar when scrolled past headerHeight - headerHeight should === #home height 
+var initialHeight = (window.innerHeight);
+$(window).bind('scroll', function() {
+    console.log(initialHeight);
+    if ($(window).scrollTop() > innerHeight) {
+        $('#myNav').removeClass('navbar-top');
+        $('#myNav').addClass('navbar-fixed-top');
+        $('#myNav').fadeIn(2000).css('display', "visible");
+    } else {
+        $('#myNav').fadeOut(2000).hide(2000);
+        // $('#myNav').removeClass('navbar-fixed-top');
+        // $('#myNav').addClass('navbar-top');
+
+    }
+});
+
+
+// Makes The Project section zoom in when scrolled past 2700
+// $(window).bind('scroll', function() {
+//         if ($(window).scrollTop() > innerHeight + 2000) {
+//             $('.portfolio-item').addClass('wow zoomInDown animated');
+//         }
+//     });
+
+// Makes The Project section zoom in when scrolled past 2700
+// $(window).bind('scroll', function() {
+//     if ($(window).scrollTop() > 3500-initialHeight) {
+//         // $('#porfolio-item').addClass('');
+//         $('.portfolio-item').css("visibility", "visible").addClass('wow zoomInDown animated');
+
+//     } 
+// });
+
+//Displays Caption on Projects when Mouse hovers 
+$(".hover").mouseleave(
+    function() {
+        $(this).removeClass("hover");
+    }
+);
